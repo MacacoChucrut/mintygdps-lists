9064804b5163fe2bf1f44194870d87848d42efdd
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2026-08-19 19:46:28 -0400

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -45,7 +45,7 @@ export default {
                         <li>You may not use any disallowed mods. A list of what we allow and don't allow can be found <a href="https://docs.google.com/spreadsheets/d/1M4vXMxHcYwtstB6SD9r4lPFotUXhz3IL9D_3JX8tjyE/edit?gid=1204643762#gid=1204643762" target="_blank">here.</a></li>
                         <li>Your record must show the stats on the "Level Complete!" and stats endscreen (attempts, orbs, etc).</li>
                         <li>You may not use any skips that make any section of the level significantly easier.</li>
-                        <li>A maximum of 2 keybinds is allowed per player.</li>
+                        <li>A maximum of 2 keybinds are allowed per player.</li>
                         <li>If a level specifies a Method and/or FPS, you must follow those parameters. If left blank, any method or FPS setup is allowed. A list of all methods can be found <a href="https://docs.google.com/document/d/1PMr1f_CiVhmBbt4OWfdwy_V5_n1AS4zIqzdhFm1VdBg/edit?usp=sharing">here.</a></li>
                         <li>Changing your FPS mid-attempt is not allowed.</li>
                     
