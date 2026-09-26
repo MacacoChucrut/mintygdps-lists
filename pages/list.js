4d9c1468d8acc21038f18896988003d8e3fa1478
MacacoChucrut <93385434+MacacoChucrut@users.noreply.github.com>
2026-09-26 12:56:25 -0300

    Update list.js

--- pages/list.js
+++ pages/list.js
@@ -89,7 +89,8 @@ export default {
 
                     <LevelAuthors 
                         :creators="level.creators" 
-                        :verifier="level.verifier">
+                        :verifier="level.verifier"
+                        :world record="level.world-record>
                     </LevelAuthors>
 
                     <div style="display:flex;">
